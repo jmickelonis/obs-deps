b4f310ba67d9ff112de488d6055ef187d8498c92
John Mickelonis <john.mickelonis@gmail.com>
2026-07-22 23:03:51 -0400

    deps.qt: Backport fix for QTBUG-146665 In OBS Studio, this bug causes right-click to not work for disabled items in the source list. This is fixed on the Qt 6.11 branch and in 6.12.0-beta1.

--- deps.qt/qt6.zsh
+++ deps.qt/qt6.zsh
@@ -10,6 +10,8 @@ local -a patches=(
     df46dc93e874c36b2ad0da746c43585528308a7fcde60930c1ffb5e841472e7b"
   "macos ${0:a:h}/patches/Qt6/mac/0002-QTBUG-147209.patch \
     a7450ba8bb0eaf0e3bc0fd66ecb6496b38015194ebbac486092e19400342de43"
+  "macos ${0:a:h}/patches/Qt6/mac/0003-QTBUG-146665.patch \
+    48120aa1118711447cddba04b3c2a19aa8c1fe91f3ed4815f8f33e759ef22634"
 )
 
 local -a qt_components=(
