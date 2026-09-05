32f98c8b1ff7cd154ad976f8eab267f7dcbf6706
John Mickelonis <jmick@email.com>
2026-09-05 00:35:04 -0400

    Updated Qt to 6.11.2.

--- deps.qt/qt6.ps1
+++ deps.qt/qt6.ps1
@@ -1,7 +1,7 @@
 param(
     [string] $Name = 'qt6',
-    [string] $Version = '6.11.1',
-    [string] $Uri = 'https://download.qt.io/archive/qt/6.11/6.11.1',
+    [string] $Version = '6.11.2',
+    [string] $Uri = 'https://download.qt.io/archive/qt/6.11/6.11.2',
     [string] $Hash = "${PSScriptRoot}/checksums",
     [array] $Targets = @('x64', 'arm64'),
     [array] $Patches = @(
